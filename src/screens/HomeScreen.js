3de755e75376bb6e32b51b76deb8b815231d444e
Mario-junior95 <karehmario@gmail.com>
2021-01-22 19:31:57 +0200

    Fixing issue in token

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,16 +3,12 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
+
+  
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
-      <Button
-        title="Log Out"
-        onPress={() => {
-          navigation.navigate("Login");
-          AsyncStorage.removeItem("token");
-        }}
-      />
+      <Button title="Log Out" onPress={() => {navigation.navigate("Login"); AsyncStorage.removeItem('token')}} />
     </View>
   );
 };
